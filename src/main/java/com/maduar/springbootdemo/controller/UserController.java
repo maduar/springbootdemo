041302e4ba65f3e726b4da17e368d6674121c86a
danny.pang <maduar86@gmail.com>
2018-06-14 16:29:41 +0800

    add poi

--- src/main/java/com/maduar/springbootdemo/controller/UserController.java
+++ src/main/java/com/maduar/springbootdemo/controller/UserController.java
@@ -5,6 +5,8 @@ import com.maduar.springbootdemo.entity.TUser;
 import com.maduar.springbootdemo.mapping.UserRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,10 +23,9 @@ public class UserController {
         return userRepository.findAll();
     }
 
-    @RequestMapping(value="/hello", method = RequestMethod.GET)
-    public String sayHello() {
-
-
+    @GetMapping(value="/hello/{custId}/")
+    public String sayHello(@PathVariable("custId") String custId) {
+        System.out.println("custId: " + custId);
         return "hello";
     }
 }

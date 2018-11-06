10e78f575b4188597760e79b8919ba3220368d6b
maduar <maduar86@gmail.com>
2018-11-06 18:02:06 +0800

    update rabbitmq config

--- src/main/java/com/maduar/springbootdemo/controller/ConfigController.java
+++ src/main/java/com/maduar/springbootdemo/controller/ConfigController.java
@@ -15,7 +15,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
  *
  * */
 @Controller
-@RequestMapping("config")
+@RequestMapping("configx")
 public class ConfigController {
 
     @Value("${useLocalCache:false}")

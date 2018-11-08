43a4a861b5eb1f3d72e384dcc6aef8fa2d717bd4
maduar <maduar86@gmail.com>
2018-11-08 10:23:41 +0800

    add nacos

--- src/main/java/com/maduar/springbootdemo/App.java
+++ src/main/java/com/maduar/springbootdemo/App.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @ComponentScan
 @EnableCaching
-@NacosPropertySource(dataId = "mmm2", autoRefreshed = true)
+@NacosPropertySource(dataId = "springbootdemo", autoRefreshed = true)
 public class App {
 
     public static void main(String[] args) throws Exception{
